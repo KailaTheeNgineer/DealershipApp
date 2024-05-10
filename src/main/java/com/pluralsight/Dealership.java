67bd2719247872973b99f724e3ee773cccf49bfe
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-05-10 15:24:37 -0400

    Got as far as I could

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,50 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+
+    ArrayList Vehicle = new ArrayList<>();
+    inventory: ArrayList<Vehicle>
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name; this.address = address; this.phone = phone;
+    }
+
+    public double getVehiclesByPrice(min, max){
+
+
+    }
+
+    public String getVehicleByMake() {
+
+    }
+    public int getVehicleByYear() {
+
+    }
+    public String getVehicleByColor() {
+
+    }
+    public int getVehicleByMileage() {
+
+    }
+    public String getVehicleByType() {
+
+    }
+
+    public Vehicle getAllVehicles() {
+
+    }
+    public Vehicle addVehicle(Vehicle) {
+
+    }
+    public Vehicle removeVehicle(Vehicle) {
+
+    }
+
+
 }
